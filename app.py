7f4091f7dfea9aba3829863e3cbf4d5147768fbc
hyh-sherry <49596048+hyh-sherry@users.noreply.github.com>
2019-08-28 00:00:33 -0400

    app

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import mission_to_mars
+
+app = Flask(__name__)
+
+app.config["MONGO_URL"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def home():
+    information = mongo.db.collection.find_one()
+    return render_template("index.html", listings = information)
+
+
+@app.route("/scrape")
+def scrape():
+    mars_data = mission_to_mars.scrape_info()
+    mongo.db.collection.update({}, mars_data, upsert=True)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+    
